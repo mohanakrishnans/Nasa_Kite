81eb3d95cc1030637a37c8951e7495a6e33e807c
MohanaKrishnan <555mohann@gmail.com>
2017-07-12 18:30:38 +0530

    LatLong from globe

--- root/js/views/GlobeViewModel.js
+++ root/js/views/GlobeViewModel.js
@@ -192,6 +192,29 @@ define(['knockout', 'jquery', 'jqueryui',
             self.onTimeSliderStop = function () {
                 self.timeSliderValue(0);
             };
+
+            globe.wwd.addEventListener('click', function (event) {
+                self.handleDropClick(event);
+                var x = event.clientX;
+                var y = event.clientY;
+                var pickList = globe.wwd.pick(globe.wwd.canvasCoordinates(x, y));
+
+
+                // var pick = globe.wwd.terrainObject(globe.wwd.canvasCoordinates(x,y));
+                 var position = pickList.objects[0].position;
+                alert("test "+ position.latitude +" "+ position.longitude) ;
+
+                   JSON.stringify(pick,null,"4");
+                    var $featuredialog =  $("#feature")
+                    .html('<iframe style="border: 0px; " src="http://localhost:8080/geoserver/topp/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=topp%3Astates&STYLES&LAYERS=topp%3Astates&INFO_FORMAT=text%2Fhtml&FEATURE_COUNT=50&X=50&Y=50&SRS=EPSG%3A4326&WIDTH=101&HEIGHT=101&BBOX=-104.1943359375%2C33.5302734375%2C-95.3173828125%2C42.4072265625" width="100%" height="100%"></iframe>')
+                    .dialog({ 
+                              autoOpen: false,
+                              title: "GetFeatureInfo"
+                            });
+                        $featuredialog.dialog("open");
+
+            });
+
         }
 
         return GlobeViewModel;
